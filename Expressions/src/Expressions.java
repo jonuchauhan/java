b88f69d73918b51b28ed1acef659d1c325128c1f
jonuchauhan <ext-jonu.chauhan@elisa.fi>
2022-12-28 10:05:14 +0200

    expressions

---
+++ Expressions/src/Expressions.java
@@ -0,0 +1,24 @@
+public class Expressions {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+        System.out.println(x);
+        x = x + 1; // addition
+        System.out.println(x);
+
+        x = x - 1;// subtraction
+        System.out.println(x);
+
+        x = x * 2;// multiplication
+        System.out.println(x);
+
+        x = x / 3;// division
+        System.out.println(x);
+
+        x = x % 4;// remainder
+
+        System.out.println(x);
+
+    }
+}
