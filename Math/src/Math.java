55d848f8ebf368a2354730bba783d283557c829a
jonuchauhan <ext-jonu.chauhan@elisa.fi>
2022-12-28 09:48:11 +0200

    math class in java

---
+++ Math/src/Math.java
@@ -0,0 +1,16 @@
+public class Math {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        int z = java.lang.Math.max(x, y);
+        System.out.println("max of x and y is : " + z);
+
+        double a = java.lang.Math.sqrt(x * x + y * y);
+
+        System.out.println("sqrt of both x and y is : " + a);
+
+    }
+
+}
