c880bf49df0fcba58141283356e6811946b1cf06
jonuchauhan <ext-jonu.chauhan@elisa.fi>
2022-12-28 10:01:10 +0200

    while loop

---
+++ WhileLoop/src/WhileLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name :");
+
+        String name = scan.nextLine();
+
+        while (name.isEmpty()) {
+            System.out.println("Enter your name :");
+            name = scan.nextLine();
+
+        }
+
+        System.out.println("Hello " + name);
+        scan.close();
+
+    }
+}
