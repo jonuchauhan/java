91bf8af5f24e09e3d25073164f18df98829b436b
jonuchauhan <ext-jonu.chauhan@elisa.fi>
2022-12-28 10:12:18 +0200

    methods

---
+++ Methods/src/Methods.java
@@ -0,0 +1,18 @@
+public class Methods {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("Sum of two given numbers is " + addition(x, y));
+
+    }
+
+    static int addition(int x, int y) {
+
+        int z = x + y;
+        return z;
+    }
+
+}
